b48825be1d5547db144da7277c90f8acb8562378
Mohamad Ansari <mansari@bu.edu>
2021-03-23 00:38:47 +0300

    Status messages added

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -11,10 +12,28 @@ class Profile(models.Model):
     city = models.TextField(blank=True)
     email_address = models.TextField(blank=True)
     image_url = models.URLField(blank=True)
+
+    def get_status_messages(self):
+        '''Takes profile messages for the Person'''
+
+        return StatusMessage.objects.filter(profile=self)
     
 
     def __str__(self):
         '''Return a string representation of the name and city'''
 
         return f'{self.first_name} {self.last_name} - {self.city}'
+
+class StatusMessage(models.Model):
+    '''Data attributes of facebook status message'''
+
+    # data attributes:
+    timestamp = models.TimeField(blank=True, auto_now=True)
+    message = models.TextField(blank=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        '''Return a string representation of the StatusMessage'''
+
+        return f'{self.timestamp}  {self.message}'
         
