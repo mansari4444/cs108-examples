ae454c4904caa87a51fd4c2a72db934668b1d8be
Mohamad Ansari <mansari@bu.edu>
2021-03-22 13:51:52 +0300

    Quotes app: added DetailView to show one quote

--- quotes/views.py
+++ quotes/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Quote
+import random
 
 class HomePageView(ListView):
     '''Show a listing of quotes'''
     model = Quote # retrieve Quote objects from the database
     template_name = "quotes/home.html"
     context_object_name = "quotes"
+
+class QuotePageView(DetailView):
+    '''Display a single quote object.'''
+    model = Quote # retrieve Quote objects from the database
+    template_name = "quotes/quote.html" # delegate the display to this template
+    context_object_name = "quote" # use this variable name in the template
+
+class RandomQuotePageView(DetailView):
+    '''Display a single quote object.'''
+    model = Quote # retrieve Quote objects from the database
+    template_name = "quotes/quote.html" # delegate the display to this template
+    context_object_name = "quote" # use this variable name in the template
+
+    def get_object(self):
+        '''Select one quote at random for display in the quote.html template'''
+
+        # obtain all quotes using the object manager
+        quotes = Quote.objects.all()
+        # select one at random
+        q = random.choice(quotes)
+        return q
 
 # Create your views here.
